c6f2847b5c722e4e6663ada11b54111722b9c3a2
暮晨 <ducheng@360.net>
2018-05-02 22:00:09 +0800

    完成日志的配置

---
+++ pocsuite/lib/core/enums.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class OUTPUT_STATUS:
+    """
+    输出信息码
+    """
+    FAILED = 0
+    SUCCESS = 1
+
+
+class ERROR_TYPE_ID:
+    """
+    输出信息错误码
+    """
+    # PoC 执行时发生 NotImplementedError 异常
+    NOTIMPLEMENTEDERROR = 2
+    # PoC 执行时发生 ConnectionError 异常
+    CONNECTIONERROR = 3.0
+    # PoC 执行时发生 HTTPError 异常
+    HTTPERROR = 3.1
+    # PoC 执行时发生 ConnectTimeout 异常
+    CONNECTTIMEOUT = 3.2
+    # PoC 执行时发生 HTTPError 异常
+    TOMANYREDIRECTS = 3.3
+    # PoC 执行时发生其他异常
+    OTHER = 4
+
+
+class CUSTOM_LOGGING:
+    """
+    日志信息
+    """
+    WARNING = 6
+    ERROR = 7
+    SUCCESS = 8
+    SYSINFO = 9
+
+
+class HTTP_HEADER:
+    ACCEPT = "Accept"
+    ACCEPT_CHARSET = "Accept-Charset"
+    ACCEPT_ENCODING = "Accept-Encoding"
+    ACCEPT_LANGUAGE = "Accept-Language"
+    AUTHORIZATION = "Authorization"
+    CACHE_CONTROL = "Cache-Control"
+    CONNECTION = "Connection"
+    CONTENT_ENCODING = "Content-Encoding"
+    CONTENT_LENGTH = "Content-Length"
+    CONTENT_RANGE = "Content-Range"
+    CONTENT_TYPE = "Content-Type"
+    COOKIE = "Cookie"
+    SET_COOKIE = "Set-Cookie"
+    HOST = "Host"
+    LOCATION = "Location"
+    PRAGMA = "Pragma"
+    PROXY_AUTHORIZATION = "Proxy-Authorization"
+    PROXY_CONNECTION = "Proxy-Connection"
+    RANGE = "Range"
+    REFERER = "Referer"
+    SERVER = "Server"
+    USER_AGENT = "User-Agent"
+    TRANSFER_ENCODING = "Transfer-Encoding"
+    URI = "URI"
+    VIA = "Via"
---
+++ pocsuite/lib/core/log.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+import sys
+
+from pocsuite.lib.core.enums import CUSTOM_LOGGING
+
+logging.addLevelName(CUSTOM_LOGGING.SYSINFO, '*')
+logging.addLevelName(CUSTOM_LOGGING.SUCCESS, '+')
+logging.addLevelName(CUSTOM_LOGGING.ERROR, '-')
+logging.addLevelName(CUSTOM_LOGGING.WARNING, '!')
+
+LOGGER = logging.getLogger('pocsuiteLog')
+
+LOGGER_HANDLER = None
+
+# 下面这一段应该是禁止多彩输出，不过我没有在help中看到相关参数，是被忘了？
+disableColor = False
+
+for argument in sys.argv:
+    if "disable-col" in argument:
+        disableColor = True
+
+        break
+
+if disableColor:
+    LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
+else:
+    try:
+        from pocsuite.thirdparty.ansistrm.ansistrm import ColorizingStreamHandler
+    except ImportError as e:
+        LOGGER_HANDLER = logging.StreamHandler(sys.stdout)
+    else:
+        LOGGER_HANDLER = ColorizingStreamHandler(sys.stdout)
+        LOGGER_HANDLER.level_map[logging.getLevelName('*')] = (None, "cyan",
+                                                               False)
+        LOGGER_HANDLER.level_map[logging.getLevelName('+')] = (None, "green",
+                                                               False)
+        LOGGER_HANDLER.level_map[logging.getLevelName('-')] = (None, "red",
+                                                               False)
+        LOGGER_HANDLER.level_map[logging.getLevelName('!')] = (None, "yellow",
+                                                               False)
+
+FORMATTER = logging.Formatter("\r[%(asctime)s] [%(levelname)s] %(message)s",
+                              "%H:%M:%S")
+
+LOGGER_HANDLER.setFormatter(FORMATTER)
+LOGGER.addHandler(LOGGER_HANDLER)
+LOGGER.setLevel(CUSTOM_LOGGING.WARNING)
